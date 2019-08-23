4be5361a5626a0f988edfa79bc8e7f799be8e077
Samuel Adakole <stagent24@gmail.com>
2019-08-23 14:37:29 +0100

    Main git

--- app/src/main/java/com/cybertech/cyberchat/HomeActivity.java
+++ app/src/main/java/com/cybertech/cyberchat/HomeActivity.java
@@ -2,14 +2,33 @@ package com.cybertech.cyberchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference reference;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+       database = FirebaseDatabase.getInstance();
+       reference = database.getReference("Users");
+       reference.keepSynced(true);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        UserAdapter adapter = new UserAdapter(HomeActivity.this,reference);
+        recyclerView.setAdapter(adapter);
     }
 
 
